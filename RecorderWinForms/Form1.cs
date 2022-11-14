b8cf717c317db1f79b50af0438d5d6dfb896a0fa
DvinaArano <37045594+DvinaArano@users.noreply.github.com>
2022-11-14 21:08:31 +0300

    main addition

---
+++ RecorderWinForms/Form1.cs
@@ -0,0 +1,94 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace RecorderWinForms
+{
+    public partial class Form1 : Form
+    {
+        // WaveIn - поток для записи
+        WaveIn waveIn;
+        //Класс для записи в файл
+        WaveFileWriter writer;
+        //Имя файла для записи
+        string outputFilename = @"D:\GitHub\csharp\demo\bin\Debug\net5.0\test_record.wav";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+            }
+            else
+            {
+                //Записываем данные из буфера в файл
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+            }
+        }
+        //Завершаем запись
+        void StopRecording()
+        {
+            MessageBox.Show("StopRecording");
+            waveIn.StopRecording();
+        }
+        //Окончание записи
+        private void waveIn_RecordingStopped(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler(waveIn_RecordingStopped), sender, e);
+            }
+            else
+            {
+                waveIn.Dispose();
+                waveIn = null;
+                writer.Close();
+                writer = null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Start Recording");
+                waveIn = new WaveIn();
+                //Дефолтное устройство для записи (если оно имеется)
+                //встроенный микрофон ноутбука имеет номер 0
+                waveIn.DeviceNumber = 0;
+                //Прикрепляем к событию DataAvailable обработчик, возникающий при наличии записываемых данных
+                waveIn.DataAvailable += waveIn_DataAvailable;
+                //Прикрепляем обработчик завершения записи
+                waveIn.RecordingStopped += waveIn_RecordingStopped;
+                //Формат wav-файла - принимает параметры - частоту дискретизации и количество каналов(здесь mono)
+                waveIn.WaveFormat = new WaveFormat(16000,16,1);
+                //Инициализируем объект WaveFileWriter
+                writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+                //Начало записи
+                waveIn.StartRecording();
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (waveIn != null)
+            {
+                StopRecording();
+            }
+        }
+    }
+}
